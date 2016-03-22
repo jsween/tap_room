2043181a3ccfada2d9329baf2595466fa173d248
Aundra Miller and Jon Sweeney <jonsweeney26@gmail.com>
2016-03-22 09:13:06 -0700

    complete the template for angular projects

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
-    <p>This is also part of the template!</p>
-    <p>Jon said 'always drink upstream from the herd'</p>
+    <div class="container center">
+      <h1>Skeleton Angular2 App!</h1>
+      <p>This is also part of the template!</p>
+      <p>Jon said 'always drink upstream from the herd'</p>
+    </div>
   `
 })
 
