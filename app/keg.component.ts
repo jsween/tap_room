429860dce34e38bb832bba2761d98d8e9b94c6c9
jon sweeney and josh overly <jon+josh@>
2016-03-23 13:31:58 -0700

    add filter

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -5,13 +5,21 @@ import { Keg } from './keg.model';
   selector: 'keg-display',
   inputs: ['keg'],
   template: `
-    <h3>{{ keg.name }}</h3>
-    <h5>Brewery: {{ keg.brand }}</h5>
-    <p>Pints Left: {{ keg.pintsLeft }}</p>
-    <p>Price Per Pint: \$\{{ keg.price }}</p>
+    <div>
+      <input *ngIf="keg.isEmpty" type="checkbox" checked (click)="toggleEmpty(true)"/>
+      <input *ngIf="!keg.isEmpty" type="checkbox" (click)="toggleEmpty(false)"/>
+      <label>{{ keg.name }}</label>
+    </div>
+      <h3>{{ keg.name }}</h3>
+      <h5>Brewery: {{ keg.brand }}</h5>
+      <p>Pints Left: {{ keg.pintsLeft }}</p>
+      <p>Price Per Pint: \$\{{ keg.price }}</p>
   `
 })
 
 export class KegComponent {
   public keg: Keg;
+  toggleEmpty(setState: boolean) {
+    this.keg.isEmpty = setState;
+  }
 }
